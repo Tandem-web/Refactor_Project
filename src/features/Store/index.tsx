c98f7ea9ed281a9a81407781f4460c0f3036e38d
Nikita Lebedev <LebedevWebProgrammer@yandex.ru>
2025-07-10 13:41:16 +0300

    make some changes

--- src/features/Store/index.tsx
+++ src/features/Store/index.tsx
@@ -1,7 +1,4 @@
-import { get_all_products } from "./api/api";
-
 function Store() {
-    get_all_products();
     return (
         <>
         </>
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import './shared/styles/index.scss';
 import AuthProvider from "./features/Auth/context/auth-provider";
+import React from "react";
 
 const root = createRoot(document.getElementById('root'));
 
